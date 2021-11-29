d3c1731ba916e78dc2573f499ee105c9a66cc4bc
Prasad Baviskar <71400522+PrasadBaviskar@users.noreply.github.com>
2021-11-29 14:57:51 +0530

    Update app.js

--- Coding-Eval-1/app.js
+++ Coding-Eval-1/app.js
@@ -12,11 +12,36 @@ const companySchema = new mongoose.Schema({
     rating:{type:Number},
     job_type:{type:String},
     vacancy:{type:Number},
+},{
+    versionKey:false,
+    timestamps:true
 })
 
 const Company =  mongoose.model("Company",companySchema)
 
-app.get("/")
+// get all jobs in a particular city which matches a particular skill
+// find all the jobs that are available as Work from home. - 
+// find all the jobs that will accept a notice period of 2 months.
+// find all jobs by sorting the jobs as per their rating.
+// an api to get details of the company. - done
+// find the company that has the most open jobs.
+
+app.get("/company/:company_name",async (req,res)=>{
+    try{
+        const company = await Company.find({company_name:req.params.company_name}).lean().exec();
+        res.status(201).send(company);
+    }
+    catch(err){
+        res.status(400).send(err);
+    }
+});
+
+app.get("/company/wfh", async (req, res) => {
+    const company = await Company.find({job_type:"Work from home"});
+    res.status(201).send(company)
+}){
+
+}
 
 const connect = () => {
     return mongoose.connect("mongodb://127.0.0.1:27017/company");
